f171387de92e8290ff2e6a5b933520f83d906cf8
kami1152 <kami1152@naver.com>
2024-05-30 11:59:13 +0900

    test git

--- src/main/java/cupet/com/demo/controller/AuthController.java
+++ src/main/java/cupet/com/demo/controller/AuthController.java
@@ -18,6 +18,7 @@ public class AuthController {
 
 	private final AuthService authService;
 
+	//test?
 	@GetMapping("/auth-token/user")
 	public Map<String, Object> AuthByUserFromMainBoot(@RequestHeader("Authorization") String authorizationHeader) {
 		System.out.println("인증컨트롤러 접근확인");
